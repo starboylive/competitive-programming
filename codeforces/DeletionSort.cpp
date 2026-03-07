1bb1e33660a0d6d9e60e9f4d8a42299b5a3f965d
Sid <thesenjubusiness@gmail.com>
2026-03-07 18:21:43 +0530

    update

--- codeforces/DeletionSort.cpp
+++ codeforces/DeletionSort.cpp
@@ -17,7 +17,7 @@ int main(){
         for(auto &x:a) cin >> x; 
         
         bool sorted = true; 
-        for(int i=0; i < n; i++){
+        for(int i=0; i < n-1; i++){
             if(a[i] > a[i+1]){
                 sorted = false; 
                 break; 
