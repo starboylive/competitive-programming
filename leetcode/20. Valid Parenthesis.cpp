f0327daab8387f729191d930eef22230beebb6ca
Sid <thesenjubusiness@gmail.com>
2026-03-12 12:30:56 +0530

    update - new questions

---
+++ leetcode/20. Valid Parenthesis.cpp
@@ -0,0 +1,27 @@
+// 20. Valid Parenthesis
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(char c : s) {
+
+            if(c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            }
+            else {
+
+                if(st.empty()) return false;
+
+                char top = st.top();
+                st.pop();
+
+                if(c == ')' && top != '(') return false;
+                if(c == '}' && top != '{') return false;
+                if(c == ']' && top != '[') return false;
+            }
+        }
+
+        return st.empty();
+    }
+};
