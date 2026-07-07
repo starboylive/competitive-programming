1b856b388cc31a447180247645b0114a0a6b0d2e
sid <161123700+starboylive@users.noreply.github.com>
2026-07-07 21:49:55 +0530

    Add Next Round [A] from Codeforces

---
+++ 158/A - Next Round/solution.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+int main() {
+    int n, k;
+    cin >> n >> k;
+ 
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+ 
+    int score = a[k - 1];
+    int count = 0;
+ 
+    for (int i = 0; i < n; i++) {
+        if (a[i] >= score && a[i] > 0) {
+            count++;
+        }
+    }
+ 
+    cout << count;
+ 
+    return 0;
+}
