a9bf03679f97b969b1e5e6758efecc7d771c805f
sid <161123700+starboylive@users.noreply.github.com>
2026-07-13 22:24:12 +0530

    Add Minimal Square [A] from Codeforces

---
+++ 1360/A - Minimal Square/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+ 
+        int mx = max(a, b);
+        int mn = min(a, b);
+ 
+        int side = max(mx, 2 * mn);
+        cout << side * side << '
+';
+    }
+ 
+    return 0;
+}
