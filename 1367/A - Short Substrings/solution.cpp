7e6dd078b7d45a229f5feb3eae29fef0dddc17f2
sid <161123700+starboylive@users.noreply.github.com>
2026-07-16 23:52:44 +0530

    Add Short Substrings [A] from Codeforces

---
+++ 1367/A - Short Substrings/solution.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        string b;
+        cin >> b;
+ 
+        string a;
+        a += b[0];
+        a += b[1];
+ 
+        for (int i = 3; i < b.size(); i += 2)
+            a += b[i];
+ 
+        cout << a << '
+';
+    }
+ 
+    return 0;
+}
