3a5fcf142d89d947be3d01ad56eb977fd683afdf
sid <161123700+starboylive@users.noreply.github.com>
2026-07-18 14:30:25 +0530

    Add Chewbaсca and Number [A] from Codeforces

---
+++ 514/A - Chewbaсca and Number/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    string s;
+    cin >> s;
+ 
+    for (int i = 0; i < s.size(); i++) {
+        int d = s[i] - '0';
+        int inv = 9 - d;
+ 
+        if (i == 0 && inv == 0)
+            continue;
+ 
+        if (inv < d)
+            s[i] = inv + '0';
+    }
+ 
+    cout << s;
+ 
+    return 0;
+}
