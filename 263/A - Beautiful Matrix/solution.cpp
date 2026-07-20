fcb970463c9a18881fc6d76e22917a15188decbf
sid <161123700+starboylive@users.noreply.github.com>
2026-07-20 11:15:27 +0530

    Add Beautiful Matrix [A] from Codeforces

--- 263/A - Beautiful Matrix/solution.cpp
+++ 263/A - Beautiful Matrix/solution.cpp
@@ -1,24 +1,18 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <bits/stdc++.h> 
+using namespace std; 
  
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+int main(){
+        int x; 
+        for(int i=0; i < 5; i++){
+            for(int j=0; j < 5; j++){
+                cin >> x; 
  
-    int row, col;
- 
-    for (int i = 1; i <= 5; i++) {
-        for (int j = 1; j <= 5; j++) {
-            int x;
-            cin >> x;
-            if (x == 1) {
-                row = i;
-                col = j;
+                    if(x==1){
+                    cout << abs(i - 2) + abs(j - 2);
+                    return 0; 
+                }
             }
         }
-    }
  
-    cout << abs(row - 3) + abs(col - 3);
- 
-    return 0;
+        return 0; 
 }
