be265bb6deba5dc2a17e522a40fa2fd6bee4a48b
sid <161123700+starboylive@users.noreply.github.com>
2026-07-20 14:45:58 +0530

    Add Soft Drinking [A] from Codeforces

---
+++ 151/A - Soft Drinking/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n, k, l, c, d, p, nl, np;
+    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
+ 
+    int drink = (k * l) / nl;
+    int lime = c * d;
+    int salt = p / np;
+ 
+    cout << min({drink, lime, salt}) / n;
+ 
+    return 0;
+}
